f07b7a36272243d43f55e6470db901b2d1338523
ashleymazzonna <ashleym5243@gmail.com>
2019-10-05 21:15:52 -0400

    add event listener for on click and keypress

--- app.js
+++ app.js
@@ -1,29 +1,39 @@
-//BUDGET DATA MODULE - Independent/Stand Alone
+
+
+//BUDGET Controller (Data Module) - Independent/Stand Alone
 var budgetController = (function () {
-    var x = 23
 
-    var add = function (a) {
-
-    }
-
-    return {
-        publicTest: function (b) {
-            console.log(add(b));
-        }
-    }
+    // some code 
 
 })();
 
 
-//UI MODULE - Independent/Stand Alone
+//UI Controller - Independent/Stand Alone
 var UIController = (function () {
-
+    // some code 
 })();
 
 
-// APP CONTROLLER -connects the UI and Data Module (ex: reads data from the UI and adds that data as a new expenses in the budgetController 
+// Global APP Controller -connects the UI and Data Module (ex: reads data from the UI and adds that data as a new expenses in the budgetController 
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var ctrlAddItem = function () {
+
+        //1. Get the field input data
+        //2. add the the new item to the budget controller
+        //3. add the item to the ui 
+        //4. calculate the budget
+        //5. display the budget on the ui
+        console.log("the enter to seems to have been pressed");
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
 })(budgetController, UIController);
 
 
