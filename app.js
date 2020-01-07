525666637180d66a59fc506651e629c722fbb027
Ashley <31573757+ashleymazzonna@users.noreply.github.com>
2020-01-07 13:50:21 -0500

    add object of query selectors

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 
-
 //BUDGET Controller (Data Module) - Independent/Stand Alone
 var budgetController = (function () {
 
-    // some code 
+    // Code 
 
 })();
 
 
 //UI Controller - Independent/Stand Alone
 var UIController = (function () {
-    // some code 
+    var DOMstrings = {
+
+        inputType = '.add__type',
+        inputDescription = '.add__description',
+        inputValue = '.add__value'
+    }
+
+    return {
+        getInput: function () {
+
+            return {
+                type: document.querySelector(DOMstrings.input).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 
@@ -20,11 +40,14 @@ var controller = (function (budgetCtrl, UICtrl) {
     var ctrlAddItem = function () {
 
         //1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
         //2. add the the new item to the budget controller
         //3. add the item to the ui 
         //4. calculate the budget
         //5. display the budget on the ui
-        console.log("the enter to seems to have been pressed");
+
     }
 
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
